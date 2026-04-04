06d2a0174f87336d9fdf698b71ed18e1a42987e3
ErbayAydoğan <ggwpyarbay123@gmail.com>
2026-04-04 21:13:51 +0300

    Update cods.py

--- src/cods.py
+++ src/cods.py
@@ -2,17 +2,18 @@ print("ÖRNEK-1 Temel Port Tarayıcı (Socket ile)")
 #Ağ Güvenliği ve Protokol Analizi" konusu ile ilgili birkaç örnek verelim ilk başta basit ve anlaşılır kısımda python ile başlayacağım temel mantığı anlayabilmek içindir.
 import socket
 
-hedef_ip = "192.168.1.1" # Kendi modeminin veya sanal makinenin IP'sini yazabilirsin örnek amaçı böyle yazdım
+hedef_ip = "192.168.1.1"
+# Kendi modeminin veya sanal makinenin IP'sini yazabilirsin örnek amaçı böyle yazdım
 aranacak_portlar = [21, 22, 80, 443] 
 
 print(f"{hedef_ip} üzerinde port taraması başlatılıyor...")
 
 for port in aranacak_portlar:
-    # IPv4 (AF_INET) ve TCP (SOCK_STREAM) için bir soket oluşturmak istiyorum
+# IPv4 (AF_INET) ve TCP (SOCK_STREAM) için bir soket oluşturmak istiyorum
     soket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     socket.setdefaulttimeout(1) # 1 saniye içinde cevap gelmezse geçsin atlasın 
     
-    # Hedef IP ve Porta bağlanmayı deneme durumu
+ # Hedef IP ve Porta bağlanmayı deneme durumu
     sonuc = soket.connect_ex((hedef_ip, port))
     
     if sonuc == 0:
