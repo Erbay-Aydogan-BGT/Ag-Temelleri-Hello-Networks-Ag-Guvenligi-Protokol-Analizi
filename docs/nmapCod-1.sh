bdc1374dd792d91a4dbb5c46bc7a1f67878c1669
ErbayAydoğan <ggwpyarbay123@gmail.com>
2026-04-05 13:34:58 +0300

    Update nmapCod-1.sh

--- docs/nmapCod-1.sh
+++ docs/nmapCod-1.sh
@@ -6,3 +6,6 @@ echo "Hedef (scanme.nmap.org) üzerinde HIZLI port taraması başlatılıyor..."
 nmap -F scanme.nmap.org
 
 echo "Tarama bitti!"
+
+# Amaç: Sadece en bilinen 100 portu tarayarak hızlıca bilgi toplamak. 
+nmap -F scanme.nmap.org
